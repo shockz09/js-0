2f7357c35062179b29373aac8a109236e16d3ca4
Rohit Mishra <87615572+shockz09@users.noreply.github.com>
2022-02-26 13:35:35 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,16 @@
+/**
+ * Write a function called solution that
+ *   takes in 3 numbers and returns the sum of the 3 input parameters
+ * @param {number} num1
+ * @param {number} num2
+ * @param {number} num3
+ * @returns {number}
+ */
+
+const solution = (num1, num2, num3) => {
+  return 0
+}
+
+module.exports = {
+  solution
+}
