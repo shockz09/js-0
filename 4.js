7f792839a55a2d6b52bbda8c1946456f2ec1a734
Rohit Mishra <87615572+shockz09@users.noreply.github.com>
2022-02-26 13:36:16 +0530

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,15 @@
+/**
+ * Write a function called solution that
+ * Takes in 2 numbers, returns the larger number
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number}
+ */
+
+const solution = (num1, num2) => {
+  return 0
+}
+
+module.exports = {
+  solution
+}
