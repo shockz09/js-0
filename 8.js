0889acfdac19b222df2a7e1844c3c2972779290d
Rohit Mishra <87615572+shockz09@users.noreply.github.com>
2022-02-26 13:37:20 +0530

    Add files via upload

---
+++ 8.js
@@ -0,0 +1,17 @@
+/**
+ * Write a function called solution that
+ * Takes in 2 numbers and returns a function
+ * The returned function returns the sum when it is called
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {function}
+ */
+
+const solution = (num1, num2) => {
+  return () => {
+  }
+}
+
+module.exports = {
+  solution
+}
