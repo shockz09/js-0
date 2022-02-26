c0849936735929a57a79bbdaee127b33ffd9130d
Rohit Mishra <87615572+shockz09@users.noreply.github.com>
2022-02-26 13:35:54 +0530

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,14 @@
+/**
+ * Write a function called solution that
+ * takes in a number and returns true if that number is greater than 5, false otherwise
+ * @param {number} num
+ * @returns {boolean}
+ */
+
+const solution = (num) => {
+  return true
+}
+
+module.exports = {
+  solution
+}
